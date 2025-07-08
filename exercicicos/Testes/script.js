7968ad60f6902763260ef34c51caa82f589182f0
Jeferson <jefersonfreitas749@gmail.com>
2025-07-08 14:20:19 -0300

    ..,

--- exercicicos/Testes/script.js
+++ exercicicos/Testes/script.js
@@ -3,21 +3,26 @@ let usuario = document.getElementById('user')
 let senha = document.getElementById('senha')
 let botao = document.getElementById('send')
 
-  function submit(e) {
-    e.preventDefault();
+const enviar = (event) => {
+    event.preventDefault()
+
 }
+
+form.addEventListener('submit', enviar)
 
 function clicar() {
-  
-
-}
-
-    if(usuario.value ==='' && senha.value === '' ) {
+    if (usuario.value === '' || senha.value === '' ) {
         
-        
+        alert('Campos vazios')
 
         
+    } else {
+      window.
     }
 
+  
+}
+
+  
 
 
