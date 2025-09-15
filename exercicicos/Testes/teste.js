743acfae8fb8d9283e2728c58f9cae047383b30a
Jeferson <jefersonfreitas749@gmail.com>
2025-09-15 00:36:38 -0300

    g

---
+++ exercicicos/Testes/teste.js
@@ -0,0 +1,15 @@
+let nome1 = "Gustavo"
+let sobrenome1 = "Silva"
+
+
+let ola = function(nome, sobrenome){
+   if(nome === undefined || sobrenome === undefined){
+   
+   } else{
+    console.log(`Olá, ${nome} ${sobrenome} tudo bem?!`)
+   }
+    return `Olá, ${nome} ${sobrenome} tudo bem?!`
+}
+
+ola()
+
